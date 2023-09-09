f55d4a86bb6318113c36e55712446754bb998cda
unknown <abooddablo@gmail.com>
2023-09-09 18:19:49 +0300

    editing

--- app/Services/TasksReorderService.php
+++ app/Services/TasksReorderService.php
@@ -8,19 +8,19 @@ use Illuminate\Http\JsonResponse;
 class TaskReorderService
 {
 
-    public function tasksReorder(int $task1, int  $task2): JsonResponse
+    public function tasksReorder(int $new_task, int  $previous_task): JsonResponse
 
     {
 
-        $task_one_db = Task::where('priority', $task1,)->first();
+        $task_one_db = Task::where('priority',$new_task,)->first();
 
-        $task_two_db = Task::where('priority', $task2)->first();
+        $task_two_db = Task::where('priority', $previous_task)->first();
 
 
         tap($task_one_db)->update(['priority' => $task_two_db->priority]);
 
 
-        tap($task_two_db)->update(['priority' => $task1]);
+        tap($task_two_db)->update(['priority' =>$new_task]);
 
 
 
